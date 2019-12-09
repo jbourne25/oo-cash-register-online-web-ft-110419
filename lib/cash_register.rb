7901f4ec19375aad960806c63f26b475e7231986
Jeremy Bourne <jbourne25@yahoo.com>
2019-12-09 17:17:41 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
-
+class CashRegister
+  
+  def initialize(total = 0)
+    @total = total
+    @discount = discount
+  end
+  
+  def discount
+    @discount
+      
+  end
+  
+  def self.total
+    @@total = current_total
+  end 
+  
+  def self.add_item(title, price, quantity)
+  end 
+    
+  
+end 
